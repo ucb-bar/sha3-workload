0adcce334699601e539289c11133ac1e1e772e2a
Nathan Pemberton <nathanp@berkeley.edu>
2019-08-29 13:38:56 -0400

    bare-metal works on spike. Not tested on rtl.

--- build.sh
+++ build.sh
@@ -7,18 +7,21 @@ mkdir -p $SPIKE_INSTALL
 
 # Build the custom spike
 if [ ! -d $SPIKE_SRC/build ]; then
+  echo "Configuring Spike:"
   mkdir -p $SPIKE_SRC/build
   pushd $SPIKE_SRC/build
   ../configure --with-fesvr=$RISCV --prefix=$SPIKE_INSTALL
   popd
 fi
 
+echo "Building Spike"
 pushd $SPIKE_SRC/build
 make -j
 make install
 popd
 
 # Build bare-metal tests
+echo "Building bare-metal tests"
 pushd bare
 make
 popd
